e1b65edba14a5757775a26f9562b0764a49442db
stas.chuhrai <stas.chuhrai@gmail.com>
2021-04-27 20:50:38 +0300

    Добавьте файлы проекта.

---
+++ Pizza_Solo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pizza_Solo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
